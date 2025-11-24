4a7a228075527d66f7f357322bf980d38f9db120
Filiz Salnur <filizsalnur@gmail.com>
2025-11-24 16:10:45 +0100

    Initial commit: Spring Boot backend setup with basic dependencies

---
+++ backend/src/main/java/com/familyflow/backend/FamilyFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.familyflow.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyFlowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyFlowBackendApplication.class, args);
+	}
+
+}
